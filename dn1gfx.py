70d3f12d94e495e057ba4b5a96bfda7a4f6720ad
Paul Guy <paulguy119@gmail.com>
2024-07-14 14:23:31 -0400

    dn1gfx.py: Operate in indexed graphics.
    Removed the ability to work on non-indexed though.  It was never very
    useful though and pillow is kinda too limited to automate the stuff
    which would be slow in python.

--- dn1gfx.py
+++ dn1gfx.py
@@ -17,6 +17,14 @@ COLORS = (0x00000000, 0x00AA0000, 0x0000AA00, 0x00AAAA00,
           0x00555555, 0x00FF5555, 0x0055FF55, 0x00FFFF55,
           0x005555FF, 0x00FF55FF, 0x0055FFFF, 0x00FFFFFF)
 
+# build palette for Image.putpalette()
+PALETTE = [0, 0, 0, 0]
+for c in COLORS:
+    PALETTE.append(c & 0x000000FF)
+    PALETTE.append((c & 0x0000FF00) >> 8)
+    PALETTE.append((c & 0x00FF0000) >> 16)
+    PALETTE.append(0xFF)
+
 # not sure if this is correct?
 PAD_SIZE = 64
 
@@ -33,10 +41,10 @@ class DN1:
 
             self.width = width_bytes * 8
 
-            self.data = array.array('I', itertools.repeat(0, self.width * self.height * self.count))
+            self.data = array.array('B', itertools.repeat(0, self.width * self.height * self.count))
 
-            bytecols = array.array('B', itertools.repeat(0, 8))
-            colors = array.array('I', itertools.repeat(0, 8))
+            colors = array.array('B', itertools.repeat(0, 8))
+            alphas = array.array('B', itertools.repeat(0, 8))
 
             # oof this is going to be slow, but the image data is tiny...
             pos = 0
@@ -44,75 +52,84 @@ class DN1:
                 for j in range(self.height):
                     rowdata = infile.read(width_bytes * 5)
                     for k in range(width_bytes):
+                        # get alphas
+                        alphas[0] = (rowdata[k * 5] & 0x80) >> 7
+                        alphas[1] = (rowdata[k * 5] & 0x40) >> 6
+                        alphas[2] = (rowdata[k * 5] & 0x20) >> 5
+                        alphas[3] = (rowdata[k * 5] & 0x10) >> 4
+                        alphas[4] = (rowdata[k * 5] & 0x08) >> 3
+                        alphas[5] = (rowdata[k * 5] & 0x04) >> 2
+                        alphas[6] = (rowdata[k * 5] & 0x02) >> 1
+                        alphas[7] = (rowdata[k * 5] & 0x01)
                         # low bit 0
-                        bytecols[0] = (rowdata[k * 5 + 1] & 0x80) >> 7
-                        bytecols[1] = (rowdata[k * 5 + 1] & 0x40) >> 6
-                        bytecols[2] = (rowdata[k * 5 + 1] & 0x20) >> 5
-                        bytecols[3] = (rowdata[k * 5 + 1] & 0x10) >> 4
-                        bytecols[4] = (rowdata[k * 5 + 1] & 0x08) >> 3
-                        bytecols[5] = (rowdata[k * 5 + 1] & 0x04) >> 2
-                        bytecols[6] = (rowdata[k * 5 + 1] & 0x02) >> 1
-                        bytecols[7] = (rowdata[k * 5 + 1] & 0x01)
+                        colors[0] = (rowdata[k * 5 + 1] & 0x80) >> 7
+                        colors[1] = (rowdata[k * 5 + 1] & 0x40) >> 6
+                        colors[2] = (rowdata[k * 5 + 1] & 0x20) >> 5
+                        colors[3] = (rowdata[k * 5 + 1] & 0x10) >> 4
+                        colors[4] = (rowdata[k * 5 + 1] & 0x08) >> 3
+                        colors[5] = (rowdata[k * 5 + 1] & 0x04) >> 2
+                        colors[6] = (rowdata[k * 5 + 1] & 0x02) >> 1
+                        colors[7] = (rowdata[k * 5 + 1] & 0x01)
                         # bit 1
-                        bytecols[0] |= (rowdata[k * 5 + 2] & 0x80) >> 6
-                        bytecols[1] |= (rowdata[k * 5 + 2] & 0x40) >> 5
-                        bytecols[2] |= (rowdata[k * 5 + 2] & 0x20) >> 4
-                        bytecols[3] |= (rowdata[k * 5 + 2] & 0x10) >> 3
-                        bytecols[4] |= (rowdata[k * 5 + 2] & 0x08) >> 2
-                        bytecols[5] |= (rowdata[k * 5 + 2] & 0x04) >> 1
-                        bytecols[6] |= (rowdata[k * 5 + 2] & 0x02)
-                        bytecols[7] |= (rowdata[k * 5 + 2] & 0x01) << 1
+                        colors[0] |= (rowdata[k * 5 + 2] & 0x80) >> 6
+                        colors[1] |= (rowdata[k * 5 + 2] & 0x40) >> 5
+                        colors[2] |= (rowdata[k * 5 + 2] & 0x20) >> 4
+                        colors[3] |= (rowdata[k * 5 + 2] & 0x10) >> 3
+                        colors[4] |= (rowdata[k * 5 + 2] & 0x08) >> 2
+                        colors[5] |= (rowdata[k * 5 + 2] & 0x04) >> 1
+                        colors[6] |= (rowdata[k * 5 + 2] & 0x02)
+                        colors[7] |= (rowdata[k * 5 + 2] & 0x01) << 1
                         # bit 2
-                        bytecols[0] |= (rowdata[k * 5 + 3] & 0x80) >> 5
-                        bytecols[1] |= (rowdata[k * 5 + 3] & 0x40) >> 4
-                        bytecols[2] |= (rowdata[k * 5 + 3] & 0x20) >> 3
-                        bytecols[3] |= (rowdata[k * 5 + 3] & 0x10) >> 2
-                        bytecols[4] |= (rowdata[k * 5 + 3] & 0x08) >> 1
-                        bytecols[5] |= (rowdata[k * 5 + 3] & 0x04)
-                        bytecols[6] |= (rowdata[k * 5 + 3] & 0x02) << 1
-                        bytecols[7] |= (rowdata[k * 5 + 3] & 0x01) << 2
+                        colors[0] |= (rowdata[k * 5 + 3] & 0x80) >> 5
+                        colors[1] |= (rowdata[k * 5 + 3] & 0x40) >> 4
+                        colors[2] |= (rowdata[k * 5 + 3] & 0x20) >> 3
+                        colors[3] |= (rowdata[k * 5 + 3] & 0x10) >> 2
+                        colors[4] |= (rowdata[k * 5 + 3] & 0x08) >> 1
+                        colors[5] |= (rowdata[k * 5 + 3] & 0x04)
+                        colors[6] |= (rowdata[k * 5 + 3] & 0x02) << 1
+                        colors[7] |= (rowdata[k * 5 + 3] & 0x01) << 2
                         # bit 3
-                        bytecols[0] |= (rowdata[k * 5 + 4] & 0x80) >> 4
-                        bytecols[1] |= (rowdata[k * 5 + 4] & 0x40) >> 3
-                        bytecols[2] |= (rowdata[k * 5 + 4] & 0x20) >> 2
-                        bytecols[3] |= (rowdata[k * 5 + 4] & 0x10) >> 1
-                        bytecols[4] |= (rowdata[k * 5 + 4] & 0x08)
-                        bytecols[5] |= (rowdata[k * 5 + 4] & 0x04) << 1
-                        bytecols[6] |= (rowdata[k * 5 + 4] & 0x02) << 2
-                        bytecols[7] |= (rowdata[k * 5 + 4] & 0x01) << 3
-                        # convert to RGB and add alpha
-                        colors[0] = COLORS[bytecols[0]] | (((rowdata[k * 5] & 0x80) >> 7) * 0xFF000000)
-                        colors[1] = COLORS[bytecols[1]] | (((rowdata[k * 5] & 0x40) >> 6) * 0xFF000000)
-                        colors[2] = COLORS[bytecols[2]] | (((rowdata[k * 5] & 0x20) >> 5) * 0xFF000000)
-                        colors[3] = COLORS[bytecols[3]] | (((rowdata[k * 5] & 0x10) >> 4) * 0xFF000000)
-                        colors[4] = COLORS[bytecols[4]] | (((rowdata[k * 5] & 0x08) >> 3) * 0xFF000000)
-                        colors[5] = COLORS[bytecols[5]] | (((rowdata[k * 5] & 0x04) >> 2) * 0xFF000000)
-                        colors[6] = COLORS[bytecols[6]] | (((rowdata[k * 5] & 0x02) >> 1) * 0xFF000000)
-                        colors[7] = COLORS[bytecols[7]] | (((rowdata[k * 5] & 0x01)) * 0xFF000000)
+                        colors[0] |= (rowdata[k * 5 + 4] & 0x80) >> 4
+                        colors[1] |= (rowdata[k * 5 + 4] & 0x40) >> 3
+                        colors[2] |= (rowdata[k * 5 + 4] & 0x20) >> 2
+                        colors[3] |= (rowdata[k * 5 + 4] & 0x10) >> 1
+                        colors[4] |= (rowdata[k * 5 + 4] & 0x08)
+                        colors[5] |= (rowdata[k * 5 + 4] & 0x04) << 1
+                        colors[6] |= (rowdata[k * 5 + 4] & 0x02) << 2
+                        colors[7] |= (rowdata[k * 5 + 4] & 0x01) << 3
+                        # palette index 0 for transparent
+                        colors[0] *= alphas[0]
+                        colors[1] *= alphas[1]
+                        colors[2] *= alphas[2]
+                        colors[3] *= alphas[3]
+                        colors[4] *= alphas[4]
+                        colors[5] *= alphas[5]
+                        colors[6] *= alphas[6]
+                        colors[7] *= alphas[7]
+                        # other indices 1-16
+                        colors[0] += alphas[0]
+                        colors[1] += alphas[1]
+                        colors[2] += alphas[2]
+                        colors[3] += alphas[3]
+                        colors[4] += alphas[4]
+                        colors[5] += alphas[5]
+                        colors[6] += alphas[6]
+                        colors[7] += alphas[7]
                         self.data[pos:pos+8] = colors[:]
                         pos += 8 # 1 byte per 8 pixels
 
     def load_image(self, filename : str):
         image = Image.open(filename)
         if image.width % 8 != 0:
-            raise ValueError("Filename width is not a multiple of 8!")
+            raise ValueError("File width is not a multiple of 8!")
         if image.width != self.width or image.height != self.height * self.count:
             raise ValueError("JSON and file dimensions don't match!")
-        self.data = array.array('I', image.tobytes())
- 
-        # this is going to be slow too...
-        for i, pixel in enumerate(self.data):
-            alpha = pixel & 0xFF000000
-            if alpha == 0:
-                if pixel & 0x00FFFFFF not in COLORS:
-                    # avoid encoding out of palette colors that wouldn't be shown anyway
-                    self.data[i] = 0
-            elif alpha == 0xFF000000:
-                if pixel & 0x00FFFFFF not in COLORS:
-                    raise ValueError("Color outside of palette!")
-            else:
-                raise ValueError("Color not fully transparent nor opaque!")
+        # pillow seems to return the highest color index as the last entry
+        if image.mode != 'P' or image.getcolors()[-1][1] > len(PALETTE) // 4:
+            raise ValueError("Image isn't indexed or has more colors than can be used!")
 
+        self.data = array.array('B', image.tobytes())
+ 
     def __init__(self, path : pathlib.Path):
         self.dn1 = False
 
@@ -133,8 +150,11 @@ class DN1:
     def save_png(self, path : pathlib.Path):
         outpng = f"{path}.png"
         outjson = f"{path}.json"
-        image = Image.frombytes('RGBA', (self.width, self.height * self.count), self.data.tobytes())
-        image.save(path, 'PNG')
+
+        image = Image.frombytes('P', (self.width, self.height * self.count), self.data.tobytes())
+        image.putpalette(PALETTE, 'RGBA')
+        image.save(outpng, 'PNG')
+
         dn1json = json.dumps({'filename': outpng, 'count': self.count, 'width': self.width, 'height': self.height})
         with open(outjson, 'w') as outfile:
             outfile.write(dn1json)
@@ -148,30 +168,43 @@ class DN1:
 
             pos = 0
             colors = array.array('B', itertools.repeat(0, 8))
+            alphas = array.array('B', itertools.repeat(0, 8))
+
             outbuf = array.array('B', itertools.repeat(0, width_bytes * 5))
             for i in range(self.count):
                 for j in range(self.height):
                     for k in range(width_bytes):
                         # at this point, alpha and color index values have been validated
 
-                        # just get the lowest alpha bit
-                        outbuf[k * 5] = (self.data[pos] & 0x01000000) >> 17
-                        outbuf[k * 5] |= (self.data[pos+1] & 0x01000000) >> 18
-                        outbuf[k * 5] |= (self.data[pos+2] & 0x01000000) >> 19
-                        outbuf[k * 5] |= (self.data[pos+3] & 0x01000000) >> 20
-                        outbuf[k * 5] |= (self.data[pos+4] & 0x01000000) >> 21
-                        outbuf[k * 5] |= (self.data[pos+5] & 0x01000000) >> 22
-                        outbuf[k * 5] |= (self.data[pos+6] & 0x01000000) >> 23
-                        outbuf[k * 5] |= (self.data[pos+7] & 0x01000000) >> 24
-                        # get colors back to EGA palette indices
-                        colors[0] = COLORS.index(self.data[pos] & 0x00FFFFFF)
-                        colors[1] = COLORS.index(self.data[pos+1] & 0x00FFFFFF)
-                        colors[2] = COLORS.index(self.data[pos+2] & 0x00FFFFFF)
-                        colors[3] = COLORS.index(self.data[pos+3] & 0x00FFFFFF)
-                        colors[4] = COLORS.index(self.data[pos+4] & 0x00FFFFFF)
-                        colors[5] = COLORS.index(self.data[pos+5] & 0x00FFFFFF)
-                        colors[6] = COLORS.index(self.data[pos+6] & 0x00FFFFFF)
-                        colors[7] = COLORS.index(self.data[pos+7] & 0x00FFFFFF)
+                        # copy colors in to buffer for operations
+                        colors[:] = self.data[pos:pos+8]
+                        # 0 - transparent, 1 - opaque
+                        alphas[0] = min(1, colors[0])
+                        alphas[1] = min(1, colors[1])
+                        alphas[2] = min(1, colors[2])
+                        alphas[3] = min(1, colors[3])
+                        alphas[4] = min(1, colors[4])
+                        alphas[5] = min(1, colors[5])
+                        alphas[6] = min(1, colors[6])
+                        alphas[7] = min(1, colors[7])
+                        # set the alpha bit
+                        outbuf[k * 5] = alphas[0] << 7
+                        outbuf[k * 5] |= alphas[1] << 6
+                        outbuf[k * 5] |= alphas[2] << 5
+                        outbuf[k * 5] |= alphas[3] << 4
+                        outbuf[k * 5] |= alphas[4] << 3
+                        outbuf[k * 5] |= alphas[5] << 2
+                        outbuf[k * 5] |= alphas[6] << 1
+                        outbuf[k * 5] |= alphas[7]
+                        # convert back to EGA values
+                        colors[0] -= alphas[0]
+                        colors[1] -= alphas[1]
+                        colors[2] -= alphas[2]
+                        colors[3] -= alphas[3]
+                        colors[4] -= alphas[4]
+                        colors[5] -= alphas[5]
+                        colors[6] -= alphas[6]
+                        colors[7] -= alphas[7]
                         # low bit 0
                         outbuf[k * 5 + 1] = (colors[0] & 0x01) << 7
                         outbuf[k * 5 + 1] |= (colors[1] & 0x01) << 6
