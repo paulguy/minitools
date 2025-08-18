f39548abcc347c28fde63a109cee90f877390124
Paul Guy <paulguy119@gmail.com>
2025-08-18 19:55:57 -0400

    vmftool.py: Rewrite a bunch of stuff.
    Not looking to make a hammer replacement, just looking to experiment,
    keep the scope limited.

--- vmftool.py
+++ vmftool.py
@@ -6,10 +6,11 @@ import pathlib
 from typing import Optional
 from enum import Enum, auto
 from dataclasses import dataclass
+from collections import namedtuple
 import pprint
 import copy
 import itertools
-import math
+from math import pi, tau, sin, cos, tan, hypot
 
 DEFAULT_MATERIAL = "BLACK_OUTLINE"
 
@@ -214,114 +215,200 @@ class SourceFile:
     def dump(data : dict[str, str | list]) -> str:
         return SourceFile.dumpclass(data)
 
-class Side:
-    plane : tuple[tuple[float, float, float],
-                  tuple[float, float, float],
-                  tuple[float, float, float]]
-    material : str = DEFAULT_MATERIAL
-    uv : tuple[tuple[float, float, float, float, float],
-               tuple[float, float, float, float, float]]
+Point2 = namedtuple('Point2', ['x', 'y'])
+Point3 = namedtuple('Point3', ['x', 'y', 'z'])
+UVPoint = namedtuple('UVPoint', ['u_x', 'u_y', 'u_z', 'u_translate', 'u_scale',
+                                 'v_x', 'v_y', 'v_z', 'v_translate', 'v_scale'])
 
-    def __init__(self, plane, material = DEFAULT_MATERIAL, uv = None):
-        self.plane = plane
-        self.material = material
-        self.uv = uv
+def translate(point : Point3,
+              pos : Point3) -> Point3:
+    return Point3(point.x + pos.x, point.y + pos.y, point.z + pos.z)
+
+def rotate(point : Point3,
+            angles : Point3) -> Point3:
+    #  1  0          0
+    #  0  cos(a_x)  -sin(a_x)
+    #  0  sin(a_x)   cos(a_x)
+
+    #  cos(a_y)  0  sin(a_y)
+    #  0         1  0
+    # -sin(a_y)  0  cos(a_y)
+
+    #  cos(a_y)              0          sin(a_y)
+    #  (sin(a_x))(sin(a_y))  cos(a_x)  -(sin(a_x))(cos(a_y))
+    # -(cos(a_x))(sin(a_y))  sin(a_x)   (cos(a_x))(cos(a_y))
+
+    #  cos(a_z)  -sin(a_z)  0
+    #  sin(a_z)   cos(a_z)  0
+    #  0          0         1
+
+    #  (cos(a_y))(cos(a_z))                                 -(cos(a_y))(sin(a_z))                                  sin(a_y)
+    #  (sin(a_x))(sin(a_y))(cos(a_z))+(cos(a_x))(sin(a_z))  -(sin(a_x))(sin(a_y))(sin(a_z))+(cos(a_x))(cos(a_z))  -(sin(a_x))(cos(a_y))
+    # -(cos(a_x))(sin(a_y))(cos(a_z))+(sin(a_x))(sin(a_z))   (cos(a_x))(sin(a_y))(sin(a_z))+(sin(a_x))(cos(a_z))   (cos(a_x))(cos(a_y))
+    # TODO this is broken
+    return Point3( (point.x * cos(angles.x) * cos(angles.z))
+                  +(point.y * sin(angles.x) * sin(angles.y) * sin(angles.z)) + (point.y * cos(angles.x) * sin(angles.z))
+                  -(point.z * cos(angles.x) * sin(angles.y) * cos(angles.z)) + (point.z * sin(angles.x) * sin(angles.z)),
+                  -(point.x * cos(angles.y) * sin(angles.z)) \
+                  -(point.y * sin(angles.x) * sin(angles.y) * sin(angles.z)) + (point.y * cos(angles.x) * cos(angles.z))
+                  -(point.z * cos(angles.x) * sin(angles.y) * sin(angles.z)) + (point.z * sin(angles.x) * cos(angles.z)),
+                   (point.x * sin(angles.y))
+                  -(point.y * sin(angles.x) * cos(angles.y))
+                  +(point.z * cos(angles.x) * cos(angles.y)))
+
+def gen_polygon(sides : int, 
+                radius : float) -> list[Point2]:
+    point_distance : float = hypot(tan(tau / sides / 2.0), 1.0) * radius
+    points : list[Point2] = []
+    for i in range(sides):
+        x : float = sin((float(i) - 0.5) / float(sides) * tau)
+        y : float = cos((float(i) - 0.5) / float(sides) * tau)
+        points.append(Point2(x * point_distance, y * point_distance))
+
+    return points
+
+class Shape:
+    points : list[Point2]
+    uvs : list[UVPoint]
+    materials : list[str]
+    thickness : float
+    top_angle : float
+    bottom_angle : float
+    children : list["Shape"]
+
+    TOP_INDEX = 0
+    BOTTOM_INDEX = 1
+    SIDE_INDEX = 2
+
+    def __init__(self, points : list[Point2],
+                       thickness : float,
+                       pos : Point3 = Point3(0.0, 0.0, 0.0),
+                       angle : Point3 = Point3(0.0, 0.0, 0.0),
+                       uvs : Optional[list[UVPoint]] = None,
+                       materials : Optional[list[str]] = None,
+                       top_angle : float = 0.0,
+                       bottom_angle : float = 0.0):
+        self.points = points
+        self.thickness = thickness
+        self.pos = pos
+        self.angle = angle
+        # TODO top/bottom angle UVs
+        if uvs is None:
+            uvs = [UVPoint(1.0, 0.0, 0.0, 0.0, 1.0,
+                           0.0, 1.0, 0.0, 0.0, 1.0),
+                   UVPoint(1.0, 0.0, 0.0, 0.0, 1.0,
+                           0.0, 1.0, 0.0, 0.0, 1.0)]
+            for i in range(len(points)):
+                p1 : Point2 = points[(i+1)%len(points)]
+                p2 : Point2 = points[i]
+                xdiff : float = p2.x - p1.x
+                ydiff : float = p2.y - p1.y
+                maxdiff : float = max(abs(xdiff), abs(ydiff))
+                uvs.append(UVPoint(xdiff / maxdiff, ydiff / maxdiff, 0.0, 0.0, 1.0,
+                                   0.0, 0.0, 1.0, 0.0, 1.0))
+        self.uvs = uvs
+        if materials is None:
+            materials = list(itertools.repeat(DEFAULT_MATERIAL, len(points) + 2))
+        self.materials = materials
+        self.children = []
+
+    def add_child(self, child : "Shape"):
+        self.children.append(child)
+
+    def set_one_material(self, idx : int, material : str):
+        self.materials[idx] = material
+
+    def set_all_materials(self, material : str):
+        self.materials = list(itertools.repeat(material, len(self.points) + 2))
+
+    def set_top_material(self, material : str):
+        self.materials[Shape.TOP_INDEX] = material
+
+    def set_bottom_material(self, material : str):
+        self.materials[Shape.BOTTOM_INDEX] = material
+
+    def set_side_material(self, idx : int, material : str):
+        self.materials[idx - Shape.SIDE_INDEX] = material
+
+    def set_all_side_materials(self, material : str):
+        self.materials[2:] = list(itertools.repeat(material, len(self.points)))
+
+    @staticmethod
+    def make_sideclass(side_id : int,
+                       p1 : Point3,
+                       p2 : Point3,
+                       p3 : Point3,
+                       uv : UVPoint,
+                       material : str) -> dict[str, str | list]:
+            sideclass : dict[str, str | list] = copy.copy(DEFAULT_SIDE)
+            sideclass['id'] = str(side_id)
+            sideclass['plane'] = f"({p1.x:.3f} {p1.y:.3f} {p1.z:.3f}) " \
+                                 f"({p2.x:.3f} {p2.y:.3f} {p2.z:.3f}) " \
+                                 f"({p3.x:.3f} {p3.y:.3f} {p3.z:.3f})"
+            sideclass['material'] = material
+            sideclass['uaxis'] = f"[{uv.u_x:.3f} {uv.u_y:.3f} {uv.u_z:.3f} {uv.u_translate:.3f}] {uv.u_scale:.3f}"
+            sideclass['vaxis'] = f"[{uv.v_x:.3f} {uv.v_y:.3f} {uv.v_z:.3f} {uv.v_translate:.3f}] {uv.v_scale:.3f}"
+            return sideclass
+
+    def to_dict(self, ids : list[int]) -> list[dict[str, str | list]]:
+        # TODO wedges
+        # TODO split concave shapes
+        shapeclasses : list[dict[str, str | list]] = []
+        shapeclass : dict[str, str | list] = {}
+        shapeclass['id'] = str(ids[0])
+        ids[0] += 1
+        sides : list[dict] = []
+        shapeclass['side'] = sides
+        # TODO select sequential points which don't all share a point on an axis
+        p1 : Point3 = translate(rotate(Point3(self.points[0].x, self.points[0].y, self.thickness / 2.0), self.angle), self.pos)
+        p2 : Point3 = translate(rotate(Point3(self.points[1].x, self.points[1].y, self.thickness / 2.0), self.angle), self.pos)
+        p3 : Point3 = translate(rotate(Point3(self.points[2].x, self.points[2].y, self.thickness / 2.0), self.angle), self.pos)
+        sides.append(Shape.make_sideclass(ids[1],
+                                          p1, p2, p3,
+                                          self.uvs[Shape.TOP_INDEX],
+                                          self.materials[Shape.TOP_INDEX]))
+        ids[1] += 1
+        p1 = translate(rotate(Point3(self.points[2].x, self.points[2].y, self.thickness / -2.0), self.angle), self.pos)
+        p2 = translate(rotate(Point3(self.points[1].x, self.points[1].y, self.thickness / -2.0), self.angle), self.pos)
+        p3 = translate(rotate(Point3(self.points[0].x, self.points[0].y, self.thickness / -2.0), self.angle), self.pos)
+        sides.append(Shape.make_sideclass(ids[1],
+                                          p1, p2, p3,
+                                          self.uvs[Shape.BOTTOM_INDEX],
+                                          self.materials[Shape.BOTTOM_INDEX]))
+        ids[1] += 1
+        for i in range(len(self.points)):
+            i2 : int = (i+1)%len(self.points)
+            p1 = translate(rotate(Point3(self.points[i2].x, self.points[i2].y, self.thickness / 2.0), self.angle), self.pos)
+            p2 = translate(rotate(Point3(self.points[i].x, self.points[i].y, self.thickness / 2.0), self.angle), self.pos)
+            p3 = translate(rotate(Point3(self.points[i].x, self.points[i].y, self.thickness / -2.0), self.angle), self.pos)
+            sides.append(Shape.make_sideclass(ids[1],
+                                              p1, p2, p3,
+                                              self.uvs[Shape.SIDE_INDEX + i],
+                                              self.materials[Shape.SIDE_INDEX + i]))
+            ids[1] += 1
+        shapeclasses.append(shapeclass)
+        # TODO figure out passing on parent transforms relative to center or face to children
+        for child in self.children:
+            shapeclasses.extend(child.to_dict(ids))
+        return shapeclasses
 
 class VMF:
     def __init__(self) -> None:
-        self.brushes : list[list[Side]] = []
+        self.shapes : list[Shape] = []
 
-    def add_brush(self, sides : list[Side]):
-        self.brushes.append(copy.copy(sides))
-
-    def add_polygon(self, pos : tuple[float, float],
-                          angle : tuple[float, float, float],
-                          sides : int,
-                          radius : float,
-                          thickness : float,
-                          materials : list[str],
-                          side_proportions : Optional[list[float]] = None,
-                          point_proportions : Optional[list[float]] = None):
-        if side_proportions is None:
-            side_proportions = list(itertools.repeat(1.0, sides))
-        if point_proportions is None:
-            point_proportions = list(itertools.repeat(1.0, sides))
-        for item in side_proportions:
-            if item > 1.0:
-                raise ValueError("side proportions must be 0.0 to 1.0")
-        max_proportion : float = sum(side_proportions)
-        proportion : float = side_proportions[0] / 2.0
-        side_points : list[tuple[float, float]] = []
-        for side in side_proportions:
-            point_distance : float = math.hypot(side, 1.0) * radius
-            x : float = math.sin(proportion / max_proportion * math.tau) * point_distance
-            y : float = math.cos(proportion / max_proportion * math.tau) * point_distance
-            side_points.append((x, y))
-            proportion += side
-        brush_sides : list[Side] = []
-        top_z : float = thickness / 2.0
-        bottom_z : float = -thickness / 2.0
-        # top
-        plane = ((side_points[0][0], side_points[0][1], top_z),
-                 (side_points[1][0], side_points[1][1], top_z),
-                 (side_points[2][0], side_points[2][1], top_z))
-        brush_sides.append(Side(plane, material=materials[0], uv=((1.0, 0.0, 0.0, 0.0, 1.0),
-                                                                  (0.0, 1.0, 0.0, 0.0, 1.0))))
-        # bottom
-        plane = ((side_points[2][0], side_points[2][1], bottom_z),
-                 (side_points[1][0], side_points[1][1], bottom_z),
-                 (side_points[0][0], side_points[0][1], bottom_z))
-        brush_sides.append(Side(plane, material=materials[1], uv=((1.0, 0.0, 0.0, 0.0, 1.0),
-                                                                  (0.0, 1.0, 0.0, 0.0, 1.0))))
-        # sides
-        for i in range(len(side_points) - 1):
-            plane = ((side_points[i+1][0], side_points[i+1][1], top_z),
-                     (side_points[i][0], side_points[i][1], top_z),
-                     (side_points[i][0], side_points[i][1], bottom_z))
-            xdiff : float = plane[1][0] - plane[0][0]
-            ydiff : float = plane[1][1] - plane[0][1]
-            maxdiff : float = max(abs(xdiff), abs(ydiff))
-            brush_sides.append(Side(plane, material=materials[i+2], uv=((xdiff / maxdiff, ydiff / maxdiff, 0.0, 0.0, 1.0),
-                                                                        (0.0, 0.0, 1.0, 0.0, 1.0))))
-        # last side
-        plane = ((side_points[0][0], side_points[0][1], top_z),
-                 (side_points[-1][0], side_points[-1][1], top_z),
-                 (side_points[-1][0], side_points[-1][1], bottom_z))
-        xdiff = plane[1][0] - plane[0][0]
-        ydiff = plane[1][1] - plane[0][1]
-        maxdiff = max(abs(xdiff), abs(ydiff))
-        brush_sides.append(Side(plane, material=materials[-1], uv=((xdiff / maxdiff, ydiff / maxdiff, 0.0, 0.0, 1.0),
-                                                                    (0.0, 0.0, 1.0, 0.0, 1.0))))
-        self.add_brush(brush_sides)
+    def add_shape(self, shape : Shape):
+        self.shapes.append(shape)
 
     def generate(self) -> str:
-        class_id = 2
-        side_id = 1
+        # world is class ID 1
+        ids = [2, 1]
         root : dict[str, str | list] = {}
         root['versioninfo'] = [copy.copy(DEFAULT_VERSIONINFO)]
         root['world'] = [copy.copy(DEFAULT_WORLD)]
         solids = []
         root['world'][0]['solid'] = solids
-        for brush in self.brushes:
-            brushclass : dict[str, str | list] = {}
-            brushclass['id'] = str(class_id)
-            class_id += 1
-            sides : list[dict] = []
-            brushclass['side'] = sides
-            solids.append(brushclass)
-            for side in brush:
-                sideclass : dict[str, str | list] = copy.copy(DEFAULT_SIDE)
-                sideclass['id'] = str(side_id)
-                side_id += 1
-                plane = side.plane
-                sideclass['plane'] = f"({plane[0][0]:.3f} {plane[0][1]:.3f} {plane[0][2]:.3f}) " \
-                                     f"({plane[1][0]:.3f} {plane[1][1]:.3f} {plane[1][2]:.3f}) " \
-                                     f"({plane[2][0]:.3f} {plane[2][1]:.3f} {plane[2][2]:.3f})"
-                sideclass['material'] = side.material
-                uv = side.uv
-                sideclass['uaxis'] = f"[{uv[0][0]:.3f} {uv[0][1]:.3f} {uv[0][2]:.3f} {uv[0][3]:.3f}] {uv[0][4]:.3f}"
-                sideclass['vaxis'] = f"[{uv[1][0]:.3f} {uv[1][1]:.3f} {uv[1][2]:.3f} {uv[1][3]:.3f}] {uv[1][4]:.3f}"
-                sides.append(sideclass)
+        for shape in self.shapes:
+            solids.extend(shape.to_dict(ids))
         return SourceFile.dump(root)
 
 def main(args : list[str]):
@@ -331,42 +418,22 @@ def main(args : list[str]):
     materials : list[str] = list(itertools.repeat("brick/brickwall026f", 10))
     materials[0] = "concrete/concretefloor033a"
     materials[1] = "concrete/concretefloor033a"
-    v.add_polygon((0.0, 0.0),
-                  (0.0, 0.0, 0.0),
-                  8,
-                  64.0,
-                  128.0,
-                  materials=materials)
+    shape : Shape = Shape(gen_polygon(8, 1024.0),
+                          16.0,
+                          materials=materials)
+    shape.add_child(Shape(gen_polygon(8, 1024.0),
+                          16.0,
+                          pos=Point3(0.0, 0.0, 256.0),
+                          materials=materials))
+    materials = list(itertools.repeat("brick/brickwall026f", 6))
+    shape.add_child(Shape(gen_polygon(4, 256.0),
+                          16.0,
+                          pos=Point3(724.0775, 724.0775, 128.0),
+                          materials=materials))
+    v.add_shape(shape)
     print(v.generate())
 
     return
- 
-    v.add_brush([Side(((-64.0, 128.0, 64.0), (64.0, 128.0, 64.0), (64.0, 0.0, 64.0)),
-                      material="concrete/concretefloor033a",
-                      uv=((1.0, 0.0, 0.0, 0.0, 0.25),
-                          (0.0, -1.0, 0.0, 0.0, 0.25))),
-                 Side(((-64.0, 0.0, 0.0), (64.0, 0.0, 0.0), (64.0, 128.0, 0.0)),
-                      material="concrete/concretefloor033a",
-                      uv=((1.0, 0.0, 0.0, 0.0, 0.25),
-                          (0.0, -1.0, 0.0, 0.0, 0.25))),
-                 Side(((-64.0, 128.0, 64.0), (-64.0, 0.0, 64.0), (-64.0, 0.0, 0.0)),
-                      material="concrete/concretefloor033a",
-                      uv=((0.0, 1.0, 0.0, 0.0, 0.25),
-                          (0.0, 0.0, -1.0, 0.0, 0.25))),
-                 Side(((64.0, 128.0, 0.0), (64.0, 0.0, 0.0), (64.0, 0.0, 64.0)),
-                      material="concrete/concretefloor033a",
-                      uv=((0.0, 1.0, 0.0, 0.0, 0.25),
-                          (0.0, 0.0, -1.0, 0.0, 0.25))),
-                 Side(((64.0, 128.0, 64.0), (-64.0, 128.0, 64.0), (-64.0, 128.0, 0.0)),
-                      material="concrete/concretefloor033a",
-                      uv=((1.0, 0.0, 0.0, 0.0, 0.25),
-                          (0.0, 0.0, -1.0, 0.0, 0.25))),
-                 Side(((64.0, 0.0, 0.0), (-64.0, 0.0, 0.0), (-64.0, 0.0, 64.0)),
-                      material="concrete/concretefloor033a",
-                      uv=((1.0, 0.0, 0.0, 0.0, 0.25),
-                          (0.0, 0.0, -1.0, 0.0, 0.25)))])
-
-    #print(v.generate())
 
 if __name__ == '__main__':
     main(sys.argv[1:])
