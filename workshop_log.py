9ae4782ad2ef9de848b9f296b79cf19112f436e3
Paul Guy <paulguy119@gmail.com>
2026-07-15 10:55:00 -0400

    Add times to returned entries.
    Also, don't use sets, so returned listings are in log order.

--- workshop_log.py
+++ workshop_log.py
@@ -2,6 +2,14 @@
 
 import sys
 import re
+import time
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class AddonEntry:
+    subscribed_ts : int
+    unsubscribed_ts : int
 
 timestamp_re = re.compile("^\\[([^\\]]+)\\] ")
 appid_re = re.compile("^\\[AppID ([^\\]]+)\\] ")
@@ -11,7 +19,6 @@ unsubscribe_re = re.compile("^Unsubscribed from item ([^ ]+)")
 url = "https://steamcommunity.com/sharedfiles/filedetails/?id="
 
 subscriptions = dict()
-unsubscriptions = dict()
 
 for filename in sys.argv[1:]:
     with open(filename, 'r') as infile:
@@ -21,7 +28,7 @@ for filename in sys.argv[1:]:
                 continue
 
             timestamp_match = timestamp_re.match(line)
-            timestamp = timestamp_match.group(1)
+            timestamp = datetime.strptime(timestamp_match.group(1), "%Y-%m-%d %H:%M:%S").timestamp()
             line = line[timestamp_match.end():].lstrip()
             appid_match = appid_re.match(line)
             if appid_match is None:
@@ -31,44 +38,51 @@ for filename in sys.argv[1:]:
 
             subscribe_match = subscribe_re.match(line)
             if subscribe_match is not None:
+                subscription = int(subscribe_match.group(1))
                 if appid not in subscriptions:
-                    subscriptions[appid] = set()
-                subscriptions[appid].add(int(subscribe_match.group(1)))
+                    subscriptions[appid] = dict()
+                if subscription not in subscriptions[appid]:
+                    subscriptions[appid][subscription] = (AddonEntry(timestamp, -1))
+                else:
+                    if timestamp > subscriptions[appid][subscription].subscribed_ts:
+                        subscriptions[appid][subscription].subscribed_ts = timestamp
                 continue
 
             unsubscribe_match = unsubscribe_re.match(line)
             if unsubscribe_match is not None:
-                if appid not in unsubscriptions:
-                    unsubscriptions[appid] = set()
-                unsubscriptions[appid].add(int(unsubscribe_match.group(1)))
+                unsubscription = int(unsubscribe_match.group(1))
+                if appid not in subscriptions:
+                    subscriptions[appid] = dict()
+                if unsubscription not in subscriptions[appid]:
+                    subscriptions[appid][unsubscription] = (AddonEntry(-1, timestamp))
+                else:
+                    if timestamp > subscriptions[appid][unsubscription].unsubscribed_ts:
+                        subscriptions[appid][unsubscription].unsubscribed_ts = timestamp
 
-subscribed = dict()
-for appid in subscriptions.keys():
-    if appid not in subscribed:
-        subscribed[appid] = set()
-for appid in unsubscriptions.keys():
-    if appid not in subscribed:
-        subscribed[appid] = set()
-for appid in subscribed.keys():
-    if appid in unsubscriptions:
-        subscribed[appid] = subscriptions[appid].difference(unsubscriptions[appid])
-    else:
-        subscribed[appid] = subscriptions[appid]
+#for appid in subscriptions.keys():
+#    subscriptions[appid] = sorted(subscriptions[appid], key=lambda x: x.subscribed_ts)
 
 print("All time subscribed")
 for appid in subscriptions.keys():
     print(f"AppID {appid}")
-    for item in subscriptions[appid]:
-        print(f"{url}{item}")
+    for addon_id in subscriptions[appid]:
+        item = subscriptions[appid][addon_id]
+        if item.subscribed_ts < 0:
+            print(f"{datetime.fromtimestamp(item.unsubscribed_ts).isoformat()}* {url}{addon_id}")
+        else:
+            print(f"{datetime.fromtimestamp(item.subscribed_ts).isoformat()} {url}{addon_id}")
+
 print("All time unsubscribed")
-for appid in unsubscriptions.keys():
+for appid in subscriptions.keys():
     print(f"AppID {appid}")
-    for item in unsubscriptions[appid]:
-        print(f"{url}{item}")
-print("Subscribed to")
-for appid in subscribed.keys():
-    if len(subscribed[appid]) == 0:
-        continue
-    print(f"AppID {appid}")
-    for item in subscribed[appid]:
-        print(f"{url}{item}")
+    for addon_id in subscriptions[appid]:
+        item = subscriptions[appid][addon_id]
+        if item.unsubscribed_ts > item.subscribed_ts:
+            print(f"{datetime.fromtimestamp(item.unsubscribed_ts).isoformat()} {url}{addon_id}")
+
+print("Currently subscribed to")
+for appid in subscriptions.keys():
+    for addon_id in subscriptions[appid]:
+        item = subscriptions[appid][addon_id]
+        if item.unsubscribed_ts < item.subscribed_ts:
+            print(f"{datetime.fromtimestamp(item.subscribed_ts).isoformat()} {url}{addon_id}")
